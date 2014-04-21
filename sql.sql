6527b0b582e093b93214186392c57a0c20b99d57
abomb4 <yangrlx@gmail.com>
2014-04-21 20:23:36 +0800

    要提交的变更： 	modified:   sql.sql

--- sql.sql
+++ sql.sql
@@ -381,7 +381,7 @@ delimiter //
  * 添加分类
  * 接收：分类名，所属父分类名
  * 返回：错误代码
- * 错误代码：0成功，1分类名已存在，2父分类已存在
+ * 错误代码：0成功，1分类名已存在，2父分类已不存在
  ******************************************************************************/
 create procedure add_category (
 	in name varchar(32),
@@ -389,9 +389,25 @@ create procedure add_category (
 	out errno int
 )
 add_category_main:begin
+	declare parent_id int unsigned;
+	if (category_existed (name)) then
+		set errno = 1;
+		leave add_category_main;
+	end if;
 	
+	select `category_id` into parent_id 
+		from `category` where `category_name` = parentname;
+	if (parentname is not null and parent_id is null) then
+		set errno = 2;
+		leave add_category_main;
+	end if;
+	
+	insert into `category` values
+		(default,parent_id,name);
+	set errno = 0;
 end//##########################添加分类结束#####################################
 
+-- 判断分类名是否存在
 create function category_existed (categoryname varchar(32))
 	returns boolean
 	DETERMINISTIC
@@ -405,13 +421,8 @@ begin
 	end if;
 	return true;
 end//
-
-create function f (
-	
-)
-	returns int
-	deterministic
-begin
-
-end//
 delimiter ;
+############################################################################
+
+SELECT * FROM `user` WHERE id <= 
+(SELECT `user_id` FROM `user` ORDER BY `user_id` desc LIMIT 10, 1) ORDER BY id desc LIMIT 10
